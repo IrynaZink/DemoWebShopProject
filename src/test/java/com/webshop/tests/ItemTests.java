13b841f8517f15e4ea277e32d36e4f625d04d0ba
IrynaZink <irynazinkovych@gmail.com>
2026-04-05 21:01:38 +0200

    HW18 added "int i = (int)" for multiply registration "

---
+++ src/test/java/com/webshop/tests/ItemTests.java
@@ -0,0 +1,13 @@
+package com.webshop.tests;
+
+public class ItemTests extends TestBase{
+    //В домашнем проекте сделайте, пожалуйста, рефакторинг тестового класса CreateAccountTests
+    //Создайте класс ItemTests и в нем метод addItemToCartTest() с предусловием,
+    // где вы авторизуетесь(login) Шаги в методе addItemToCartTest(): кликнуть на кнопку второго товара Add to cart;
+    // кликнуть на ссылку Shopping cart в хэдере; проверить добавление товара в корзину по его названию(имени)
+    public void addItemToCartTest(){
+
+    }
+
+
+}
